834bcf8fb05926e80b3c93e81d43a16e06947f73
Rodrigo "Zackstabz" Medeiros <63007829+Zackstabz@users.noreply.github.com>
2023-11-27 10:58:50 -0300

    Create BásicoMédiaC++.cpp

---
+++ Estudos em C++/BásicoMédiaC++.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double x, y, media;
+
+    cout << "Digite o primeiro número: ";
+    cin >> x;
+    cout << "Digite o segundo número: ";
+    cin >> y;
+    media = (x + y) / 2.0;
+    cout << "Media = " << media << endl;
+    return 0;
+}
