31d87eb27020ecfed8db13959c3f66f7bb78e5c5
Rodrigo "Zackstabz" Medeiros <63007829+Zackstabz@users.noreply.github.com>
2023-11-27 12:37:14 -0300

    Create BásicaMédiaPHP.php

---
+++ Estudos em PHP/BásicaMédiaPHP.php
@@ -0,0 +1,8 @@
+<?php
+    print "Digite o primeiro número: ";
+    $x = trim(fgets(STDIN));
+    print "Insira o segundo número: "/
+    $y = trim(fgets(STDIN));
+    $avarage = ($x + $y) / 2;
+    print "Média = $average";
+?>
