7d34ee63786f69bc2f818ad6b3232bfdee8078cf
Rodrigo "Zackstabz" Medeiros <63007829+Zackstabz@users.noreply.github.com>
2023-12-01 09:54:54 -0300

    Create Float_ExpressaoJS.java

---
+++ Estudos em JS/Float_ExpressaoJS.java
@@ -0,0 +1,19 @@
+package teste_processamento;
+
+public class float_expressao {
+
+	public static void main(String[] args) {
+		
+		float b, B, h, area;
+		
+		b = 6f;
+		B = 8f;
+		h = 5f;
+		
+		area = (b + B) / 2f * h;
+		
+		System.out.println(area);
+
+	}
+
+}
