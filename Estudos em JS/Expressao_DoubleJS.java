717b2288234a8228023dbfa1b85db5954cac8255
Rodrigo "Zackstabz" Medeiros <63007829+Zackstabz@users.noreply.github.com>
2023-12-01 09:53:54 -0300

    Create Expressao_DoubleJS.java

---
+++ Estudos em JS/Expressao_DoubleJS.java
@@ -0,0 +1,19 @@
+package teste_processamento;
+
+public class Double_Expressao {
+
+	public static void main(String[] args) {
+		
+		double b, B, h, area;
+		
+		b = 6.0;
+		B = 8.0;
+		h = 5.0;
+		
+		area = (b + B) / 2.0 * h;
+		
+		System.out.println(area);
+
+	}
+
+}
