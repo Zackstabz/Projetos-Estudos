5ced08a9f8e14ef050a90be4333f696de6ed3937
Rodrigo "Zackstabz" Medeiros <63007829+Zackstabz@users.noreply.github.com>
2023-12-01 10:02:50 -0300

    Create Casting2_ProcessamentoJS.java

---
+++ Estudos em JS/Casting2_ProcessamentoJS.java
@@ -0,0 +1,17 @@
+package aula14_teste_processamento;
+
+public class exemplo3_processamento {
+
+	public static void main(String[] args) {
+		
+		double a;
+		int b;
+		
+		a = 5.0;
+		b = (int) a;
+		
+		System.out.println(b);
+
+	}
+
+}
