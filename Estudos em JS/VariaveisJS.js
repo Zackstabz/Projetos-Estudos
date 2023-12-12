ce127a887132988886826872fa903f7d9f2e008f
Rodrigo "Zackstabz" Medeiros <63007829+Zackstabz@users.noreply.github.com>
2023-12-12 13:46:53 -0300

    Create VariaveisJS.js

---
+++ Estudos em JS/VariaveisJS.js
@@ -0,0 +1,6 @@
+//pokemon
+let nomePokemon = "Gengar" //variável tipo String
+let nivelPokemon = 20 //variável tipo Number
+let pontosHpPokemon = 45
+let sexoPokemon = "M"
+let selecionavelPokemon = true //variável tipo Boolean
