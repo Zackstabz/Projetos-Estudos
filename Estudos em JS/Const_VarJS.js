17d9bdf7215ea70ae35bd5d895b23fb44477ab6f
Rodrigo "Zackstabz" Medeiros <63007829+Zackstabz@users.noreply.github.com>
2023-12-12 13:23:03 -0300

    Create Const_VarJS.js

---
+++ Estudos em JS/Const_VarJS.js
@@ -0,0 +1,18 @@
+let poteCafe = "Café Pilão";
+let poteAcucar = "Açúcar Cristal";
+let poteBiscoito = "Biscoito Maizena";
+
+console.log("Na cozinha da vovó tem: " + 
+  poteCafe + " - " +
+  poteAcucar + " - " +
+  poteBiscoito
+);
+
+let poteCafeOriginal = poteCafe;
+poteCafe = "Café 3 corações";
+
+console.log("Na cozinha da vovó tem: " + 
+  poteCafe + " - " +
+  poteAcucar + " - " +
+  poteBiscoito
+);
