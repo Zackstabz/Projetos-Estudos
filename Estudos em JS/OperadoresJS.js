86ced3510f43b278e70422a235934b00e5544d29
Rodrigo "Zackstabz" Medeiros <63007829+Zackstabz@users.noreply.github.com>
2023-12-16 15:42:28 -0300

    Create OperadoresJS.js

---
+++ Estudos em JS/OperadoresJS.js
@@ -0,0 +1,10 @@
+// guardar o valor em uma variável de resultado TRUE?FALSE
+let marca = "Apple"
+let resultado = marca !== "Samsung"
+console.log(marca !== "Samsung")
+console.log(resultado)
+
+// = é atribuição
+// == é para comparar o valor
+// === é para comparar o valor e o formato do conteúdo
+// !== é diferente ?
